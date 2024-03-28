05c4ec982c94098a3bc1e7043429f3002cdfd43c
Fernando Mumbach <fermuch@ayvu.net>
2024-03-28 09:38:56 -0300

    added metadata to demo script

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,44 @@
-// Write your script here!
-print("hello world!");
+import { JSONSchema } from "json-schema-to-ts";
+
+const settingsSchema = {
+  type: "object",
+  additionalProperties: false,
+  required: ["email", "agrees"],
+  properties: {
+    email: {
+      type: "string",
+      format: "email",
+    },
+    agrees: {
+      type: "boolean",
+      default: false,
+    },
+  },
+} as const satisfies JSONSchema;
+
+pplus.set_script_metadata({
+  name: "Demo script",
+  description:
+    "This is a demo script. It does nothing, but showcases how to set up the scripting environment!",
+  version: "0.0.1",
+  settingsForm: [
+    {
+      $el: "h2",
+      if: "true",
+      children: "Welcome to scripts!",
+    },
+    {
+      $formkit: "primeInputText",
+      name: "email",
+      label: "Email",
+      help: "This isn't even used. Just for shows!",
+      validation: "required|email",
+    },
+    {
+      $formkit: "primeCheckbox",
+      name: "agrees",
+      label: "Are you agreeing?",
+    },
+  ],
+  settingsSchema: settingsSchema,
+});
